adbc12f6fbea8047ceefa5c3704f61dd780188d4
Jerry <jerrytodd2005@gmail.com>
2024-12-19 13:36:37 -0600

    Update installer script

--- installer.lua
+++ installer.lua
@@ -1,20 +1,22 @@
-
-
 function Write_File(path, data)
-    
     fs.delete("installer.lua")
 
     local file = fs.open(path, "w")
 
     if file then
         file.write(data) -- Write content to the file
-        file.close()                        -- Close the file
+        file.close()     -- Close the file
         print("File created successfully!")
     else
         print("Failed to create the file.")
     end
 end
 
+function Github_Download(path, filename)
+    local file = http.get("https://raw.githubusercontent.com/Jerry-Todd/Computer-Craft-Scripts/main/" .. filename).readAll()
+    Write_File("filename", file)
+end
 
-
-print("Hello World!")
+term.clear()
+print("Downloading Unity assets.")
+-- Github_Download()
