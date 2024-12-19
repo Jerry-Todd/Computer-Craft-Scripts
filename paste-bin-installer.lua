87709f18252df36670402984fce2a9c36c3aabe8
Jerry <jerrytodd2005@gmail.com>
2024-12-19 13:26:38 -0600

    Update pastbin installer

--- paste-bin-installer.lua
+++ paste-bin-installer.lua
@@ -1,27 +1,19 @@
-
-
 function Write_File(path, data)
-    
     fs.delete("installer.lua")
 
     local file = fs.open(path, "w")
 
     if file then
         file.write(data) -- Write content to the file
-        file.close()                        -- Close the file
+        file.close()     -- Close the file
         print("File created successfully!")
     else
         print("Failed to create the file.")
     end
 end
 
+Installer = http.get("https://raw.githubusercontent.com/Jerry-Todd/Computer-Craft-Scripts/main/installer.lua").readAll()
 
-
-Installer = http.get("https://raw.githubusercontent.com/Jerry-Todd/Computer-Craft-Scripts/main/installer.lua")
 Write_File("installer.lua", Installer)
 
-print(Installer)
-
 os.run({}, "installer.lua")
-
-
