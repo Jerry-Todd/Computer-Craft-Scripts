85b110df622ddeb8323db2281df40ec430795f9b
Jerry Todd <jerrytodd2005@gmail.com>
2025-01-03 15:08:28 -0600

    update again

--- unity-rewrite.lua
+++ unity-rewrite.lua
@@ -1,15 +1,17 @@
-
 local termWidth, termHeight = term.getSize() -- Get terminal size
 local statusBar = window.create(term.native(), 1, termHeight, termWidth, 1)
 local terminal = window.create(term.native(), 1, 1, termWidth, termHeight - 1)
 
+coroutine.resume(Unity)
 term.redirect(terminal)
 shell.clear()
 
-
-Unity = coroutine.create(function ()
-    local previousTerm = term.redirect(statusBar)
-    term.setBackgroundColor(colors.white)
-    term.clear()
-    term.redirect(previousTerm)
+Unity = coroutine.create(function()
+    while true do
+        local previousTerm = term.redirect(statusBar)
+        term.setBackgroundColor(colors.white)
+        term.clear()
+        term.redirect(previousTerm)
+        coroutine.yield()
+    end
 end)
