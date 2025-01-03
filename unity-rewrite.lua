1445a3d57b43b84932c1cb148fc4c1008bac4cc6
Jerry Todd <jerrytodd2005@gmail.com>
2025-01-03 14:39:35 -0600

    attempt fix again

--- unity-rewrite.lua
+++ unity-rewrite.lua
@@ -3,4 +3,4 @@ local termWidth, termHeight = term.getSize() -- Get terminal size
 local statusBar = window.create(term.native(), 1, termHeight, termWidth, 1)
 
 
-statusBar.setBackgroundColor(colors.white)
+term.setBackgroundColor(colors.white)
