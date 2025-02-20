4ee1b1ca0b244dbaae740cd45f02a0b53c78a249
Jerry <jerrytodd2005@gmail.com>
2025-02-19 21:00:06 -0600

    update installer

--- scripts/install/installer.lua
+++ scripts/install/installer.lua
@@ -28,10 +28,11 @@ end
 
 term.clear()
 term.setCursorPos(1, 1)
+fs.delete("scripts")
 
 print("Creating shortcuts")
 
-Write_File("scripts/options/update-scripts.lua", "shell.run(\"scripts/install/installer.lua\")")
+Write_File("scripts/options/update.lua", "shell.run(\"scripts/install/installer.lua\")")
 Write_File("scripts/options/exit.lua", "term.clear()\nterm.setCursorPos(1,1)")
 Write_File("startup.lua", "shell.run(\"launch.lua\")")
 
