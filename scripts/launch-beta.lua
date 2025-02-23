5481cbac015ad48e26386a60f4f387a9063efca5
Jerry <jerrytodd2005@gmail.com>
2025-02-23 10:58:09 -0600

    update

--- scripts/launch-beta.lua
+++ scripts/launch-beta.lua
@@ -1,27 +1,27 @@
--- version: 3
+-- version: 4
 
 local term_width, term_height = term.getSize()
 local max_options = term_height / 2 - 1
 
-local scripts_folder = fs.list("scripts")
-local options_folder = fs.list("scripts/options")
-
 local options = {}
 local options_path = {}
 
-for i, o in ipairs(options_folder) do
-    if string.sub(o, -4) == ".lua" then
-        table.insert(options, o)
-        table.insert(options_path, "scripts/options/" .. o)
-    end
-end
+-- local scripts_folder = fs.list("scripts")
+-- local options_folder = fs.list("scripts/options")
 
-for i, s in ipairs(scripts_folder) do
-    if string.sub(s, -4) == ".lua" then
-        table.insert(options, s)
-        table.insert(options_path, "scripts/" .. s)
-    end
-end
+-- for i, o in ipairs(options_folder) do
+--     if string.sub(o, -4) == ".lua" then
+--         table.insert(options, o)
+--         table.insert(options_path, "scripts/options/" .. o)
+--     end
+-- end
+
+-- for i, s in ipairs(scripts_folder) do
+--     if string.sub(s, -4) == ".lua" then
+--         table.insert(options, s)
+--         table.insert(options_path, "scripts/" .. s)
+--     end
+-- end
 
 for i = 1, 20 do
     table.insert(options, "dummy option " .. i)
@@ -36,7 +36,7 @@ function LoadPage()
     term.setCursorPos(2, 2)
     print("Launcher - Page " .. Page)
     for i = 1, max_options do
-        local o_num = (Page * max_options - 1) + i
+        local o_num = ((Page - 1) * max_options) + i
         if options[o_num] == nil then
             break
         end
@@ -46,13 +46,8 @@ function LoadPage()
     end
 end
 
--- for i, o in ipairs(options) do
---     term.setCursorPos(4, 2 + i * 2)
---     print(string.sub(o, 1, #o-4))
--- end
-
 function SetCursor(icon)
-    term.setCursorPos(2, 2 + ((Page - 1) * max_options) + Cursor)
+    term.setCursorPos(2, 2 + ((Page - 1) * max_options) + Cursor * 2)
     io.write(icon)
 end
 
