906fefb9bed9e50e10d03c81e347065d3a05b7a9
Jerry <jerrytodd2005@gmail.com>
2025-08-05 15:04:10 -0500

    update chests module

--- chestman/modules/chests.lua
+++ chestman/modules/chests.lua
@@ -6,11 +6,20 @@ function M.getChests()
     if #containers == 0 then
         error('No containers connected', 0)
     end
+
+    return containers
 end
 
 function M.GetAllItems()
+    local containers = M.getChests()
     for i, c in pairs(containers) do
-        print(c.list())
+        local chestName = peripheral.getName(c)
+        print(" - Chest: " .. chestName)
+        local items = c.list()
+        for slot, item in pairs(items) do
+            print("  Slot " .. slot .. ": " .. item.name .. " x" .. item.count)
+        end
+        print() -- Empty line for better readability between chests
     end
 end
 
