cdb3588b0c6e7e085331d90a958c635cc19a6476
Jerry <jerrytodd2005@gmail.com>
2025-08-05 15:00:12 -0500

    update chests

--- chestman/modules/chests.lua
+++ chestman/modules/chests.lua
@@ -1,20 +1,16 @@
-
 local M = {}
 
--- function M.getChests()
-    
-
---     return chests
--- end
-
-function M.GetAllItems()
+function M.getChests()
     local containers = { peripheral.find('minecraft:chest') }
 
     if #containers == 0 then
         error('No containers connected', 0)
     end
+end
+
+function M.GetAllItems()
     for i, c in pairs(containers) do
-        print((c.list().tostring))
+        print(c.list())
     end
 end
 
