c78efd488f2f42d4129ea34a4058221c8c251bb4
Jerry <jerrytodd2005@gmail.com>
2025-11-18 00:59:39 -0600

    Add alot to the sandbox thing

--- testing/secure.lua
+++ testing/secure.lua
@@ -20,6 +20,7 @@ end
 
 local customShell = copy(shell)
 local customFs = copy(fs)
+local customPeripheral = copy(peripheral)
 
 function prompt(...)
     local c = term.getTextColor()
@@ -41,20 +42,46 @@ end
 
 customShell.run = function(command, ...)
     local args = {...}
-    if prompt(
-        "Program attempts to run:",
-        command .. " " .. table.concat(args, " ")
-    ) then
-        shell.run(command, table.unpack(args))
+    
+    -- Special handling for "wget run"
+    if command == "wget" and args[1] == "run" and args[2] then
+        if prompt(
+            "Program attempts to download and run:",
+            args[2]
+        ) then
+            -- Download to a temporary file
+            local tempFile = ".secure_temp_" .. os.epoch("utc")
+            local success = shell.run("wget", args[2], tempFile)
+            
+            if success and fs.exists(tempFile) then
+                -- Run in secure environment
+                os.run(env, tempFile)
+                -- Clean up
+                fs.delete(tempFile)
+            else
+                print("Download failed")
+                return false
+            end
+        else
+            return false
+        end
     else
-        return false
+        -- Normal command handling
+        if prompt(
+            "Program attempts to run:",
+            command .. " " .. table.concat(args, " ")
+        ) then
+            shell.run(command, table.unpack(args))
+        else
+            return false
+        end
     end
 end
 
 customFs.makeDir = function(...)
     local args = {...}
     if prompt(
-        "Program attempts makeDir with:",
+        "Program attempts fs.makeDir with:",
         table.concat(args, " ")
     ) then
         return fs.makeDir(...)
@@ -63,15 +90,74 @@ customFs.makeDir = function(...)
     end
 end
 
+customFs.move = function(...)
+    local args = {...}
+    if prompt(
+        "Program attempts fs.move with:",
+        table.concat(args, " ")
+    ) then
+        return fs.move(...)
+    else
+        return false
+    end
+end
+
+customFs.copy = function(...)
+    local args = {...}
+    if prompt(
+        "Program attempts fs.copy with:",
+        table.concat(args, " ")
+    ) then
+        return fs.copy(...)
+    else
+        return false
+    end
+end
+
+customFs.delete = function(...)
+    local args = {...}
+    if prompt(
+        "Program attempts fs.delete with:",
+        table.concat(args, " ")
+    ) then
+        return fs.delete(...)
+    else
+        return false
+    end
+end
+
+customFs.open = function(...)
+    local args = {...}
+    if prompt(
+        "Program attempts fs.open with:",
+        table.concat(args, " ")
+    ) then
+        return fs.open(...)
+    else
+        return false
+    end
+end
+
+customPeripheral.call = function(...)
+    local args = {...}
+    if prompt(
+        "Program attempts peripheral.call with:",
+        table.concat(args, " ")
+    ) then
+        return peripheral.call(...)
+    else
+        return false
+    end
+end
+
 local env = {
     shell = customShell,
-    fs = customFs
+    fs = customFs,
+    peripheral = customPeripheral,
+    require = require
 }
-setmetatable(env, { __index = _G })
 local filename = args[1]
 if not filename:match("%.lua$") then
     filename = filename .. ".lua"
 end
-local fn = loadfile(filename)
-setfenv(fn, env)
-fn()
+os.run(env, filename)
